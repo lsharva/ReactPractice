ea4611b39e642293cc6670911b4509ccc10e18f4
lsharva <react01lsharva@gmail.com>
2023-03-09 11:06:09 +0530

    lifecycle b imported

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 // import Component01 from './ComponentFolder/Component01';
 import LifecycleA from './ComponentFolder/LifecycleA';
 import Formcomp from './Formcopm';
+import LifecycleB from './ComponentFolder/LifecycleB';
 
 function App() {
   return (
     <div>
       <LifecycleA/>
       <Formcomp/>
+      <LifecycleB/>
       {/* <Component01/> */}
     </div>  
 
